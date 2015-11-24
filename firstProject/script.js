d0890bbcf553f3a022e80eb2a58522b2bd04f79c
snilbog <fitch.neal@gmail.com>
2015-11-24 15:52:36 -0800

    working blackjack

--- firstProject/script.js
+++ firstProject/script.js
@@ -1,1 +1,6 @@
-script.js
+$(window).load(function(){
+ $('.container').find('img').each(function(){
+  var imgClass = (this.width/this.height > 1) ? 'wide' : 'tall';
+  $(this).addClass(imgClass);
+ })
+})
